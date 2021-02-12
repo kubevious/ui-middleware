4dbdbfa4af7666bfabb1e71e497bc49e03da05d2
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-02-11 19:05:15 -0800

    api fix

--- src/services/misc.ts
+++ src/services/misc.ts
@@ -2,7 +2,7 @@ import { Promise } from 'the-promise';
 import { IService } from '@kubevious/ui-framework'
 
 export interface IMiscService extends IService {
-    fetchAbout() : Promise<any>;
+    fetchAbout(cb: (data: any) => any) : void;
 
     fetchNotifications(cb: (data: any) => any) : void;
     submitFeedback(data: any, cb: (data: any) => any) : void;
