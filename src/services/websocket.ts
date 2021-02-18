a8e32c2c66fa9f8d9a74827008d46fdd19b07cc8
Semyon Pukhov <semyonpuchof@gmail.com>
2021-02-18 17:20:07 +0300

    Scope fix

--- src/services/websocket.ts
+++ src/services/websocket.ts
@@ -2,6 +2,6 @@ import { IService } from '@kubevious/ui-framework'
 
 export interface IWebSocketService extends IService {
     subscribe(target: any, cb: (data: any) => any) : any;
-    scope(cb: (data: any) => any) : any;
+    scope(cb: (value: any, target?: any) => any) : any;
 }
 
