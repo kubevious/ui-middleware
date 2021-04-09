de6b65aaf7b411ba6070b19493be0f207a71ee7e
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-04-09 14:29:52 -0700

    added subscribe APIs

--- src/services/marker.ts
+++ src/services/marker.ts
@@ -1,5 +1,48 @@
 import { IService } from '@kubevious/ui-framework'
 
+
+export type MarkerStatus = {
+    script?: string;
+    target?: string;
+    name?: string;
+    propagate?: boolean;
+    shape?: string;
+    color?: string;
+    item_count?: number;
+    error_count?: number;
+    enabled?: boolean;
+    is_current?: boolean;
+};
+
+export type DnOptions = {
+    relativeTo?: string;
+};
+
+export interface MarkerItem {
+    dn: string;
+    id?: number;
+    errors?: number;
+    warnings?: number;
+    options?: DnOptions;
+    markers?: string[];
+};
+
+export type MarkerLog = {
+    kind: string;
+    msg: {
+        source: string[];
+        msg: string;
+    };
+};
+
+export type MarkerResult = {
+    name?: string;
+    items: MarkerItem[];
+    item_count: number;
+    is_current?: boolean;
+    logs: MarkerLog[];
+};
+
 export interface IMarkerService extends IService {
     backendFetchMarkerList(cb: (data: any) => any) : void;
     backendFetchMarker(id: string, cb: (data: any) => any) : void;
@@ -7,4 +50,8 @@ export interface IMarkerService extends IService {
     backendDeleteMarker(id: string, cb: (data: any) => any) : void;
     backendExportItems(cb: (data: any) => any) : void;
     backendImportItems(markers: any, cb: (data: any) => any) : void;
+
+    subscribeMarkerStatuses(cb: ((items: MarkerStatus[]) => void)) : void;
+    subscribeMarkerResult(name: string | null, cb: ((result: MarkerResult) => void)) : void;
 }
+
--- src/services/rule.ts
+++ src/services/rule.ts
@@ -1,5 +1,50 @@
 import { IService } from '@kubevious/ui-framework'
 
+export type RuleStatus = {
+    script?: string;
+    target?: string;
+    name?: string;
+    propagate?: boolean;
+    shape?: string;
+    color?: string;
+    item_count?: number;
+    error_count?: number;
+    enabled?: boolean;
+    is_current?: boolean;
+};
+
+
+export type DnOptions = {
+    relativeTo?: string;
+};
+
+
+export interface RuleItem {
+    dn: string;
+    id?: number;
+    errors?: number;
+    warnings?: number;
+    options?: DnOptions;
+    markers?: string[];
+}
+
+export type RuleLog = {
+    kind: string;
+    msg: {
+        source: string[];
+        msg: string;
+    };
+};
+
+export type RuleResult = {
+    name?: string;
+    items: RuleItem[];
+    item_count: number;
+    is_current?: boolean;
+    logs: RuleLog[];
+};
+
+
 export interface IRuleService extends IService {
     backendFetchRuleList(cb: (data: any) => any) : void;
     backendFetchRule(id: string, cb: (data: any) => any) : void;
@@ -7,5 +52,8 @@ export interface IRuleService extends IService {
     backendDeleteRule(id: string, cb: (data: any) => any) : void;
     backendExportItems(cb: (data: any) => any) : void;
     backendImportItems(rules: any, cb: (data: any) => any) : void;
+
+    subscribeRuleStatuses(cb: ((items: RuleStatus[]) => void)) : void;
+    subscribeRuleResult(name: string | null, cb: ((result: RuleResult) => void)) : void;
 }
 
