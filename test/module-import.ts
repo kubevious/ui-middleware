b1399d930af8b80a8e675fd8055724b4caa8f7b8
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-07-21 15:49:54 -0700

    build fix

--- test/module-import.ts
+++ test/module-import.ts
@@ -2,8 +2,10 @@ import 'mocha';
 import should = require('should');
 
 import { IDiagramService } from '../src';
+import { IHistoryService } from '../src';
 
 describe('First test', () => {
     it('function', () => {
+
     });
 });
