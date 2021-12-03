4aa5b6c0cabaab8b1dbe1fc298f16797b80d7f85
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-12-02 16:25:08 -0800

    added clusters service

---
+++ src/services/cluster.ts
@@ -0,0 +1,23 @@
+import { IService } from '../common-types'
+
+export interface IClusterService extends IService {
+
+    // fetchClusterLatestSnapshot(clusterId: string);
+
+    // fetchClusterSnapshots(clusterId: string);
+
+    // fetchClusterConfig(clusterId: string);
+
+    // createClusterConfig(clusterId: string, config: ClusterConfig);
+
+    // deleteCluster(clusterId: string);
+
+    // fetchClusterToken(clusterId: string);
+
+    // resetClusterToken(clusterId: string);
+
+    // fetchClusterReportStatus(clusterId: string);
+
+    // subscribeClusterReportStatus(clusterId: string, cb: ((value: ClusterReportingStatus) => void));
+
+}
