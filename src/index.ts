01df2568e7fdfe0755f3dd812a3cb62ef9125a11
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-06 12:35:44 -0700

    added worldvious service

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export { IDiagramBrowserService } from './services/diagram-browser'
 export { ISearchService } from './services/search'
 export { IMarkerService } from './services/marker'
 export { IRuleService } from './services/rule'
-export { IMiscService } from './services/misc'
+export { IWorldviousService } from './services/worldvious'
 export { IHistoryService } from './services/history'
 export { IBackendStatusService, BackendMetricItem, BackendMetricsResponse, BackendVersionResponse } from './services/backend-status-service';
 
---
+++ src/services/worldvious.ts
@@ -0,0 +1,73 @@
+import { Promise } from 'the-promise';
+import { IService } from '../common-types'
+
+export interface IWorldviousService extends IService {
+
+    getNotificationInfo() : Promise<WorldviousNotificationsInfo>;
+    subscribeToNotificationInfo(cb: (data: WorldviousNotificationsInfo) => void) : void;
+
+    getNotifications() : Promise<WorldviousVersionInfoResult>;
+
+    submitFeedback(data: WorldviousFeedbackSubmitData) : Promise<void>;
+    submitSnooze(data: WorldviousFeedbackSnoozeData) : Promise<void>;
+
+}
+
+export enum WorldviousNotificationKind {
+    newVersion = 'new-version',
+    feedbackRequest = 'feedback-request',
+    message = 'message',
+}
+
+export interface WorldviousNewVersionInfo {
+    kind: WorldviousNotificationKind;
+    name: string;
+    version: string;
+    changes: string[];
+    features: string[];
+    url: string;
+}
+export interface WorldviousFeedbackQuestion {
+    kind: WorldviousNotificationKind;
+    id: string;
+    text: string;
+    options?: string;
+}
+export interface WorldviousFeedbackRequest {
+    kind: WorldviousNotificationKind;
+    id: string;
+    questions: WorldviousFeedbackQuestion[];
+}
+export interface WorldviousMessageData {
+    kind: WorldviousNotificationKind;
+    id: string;
+    title: string;
+    content: string;
+}
+export interface WorldviousFeedbackSubmitData {
+    id: string;
+    kind: string;
+    answers: WorldviousAnswer[];
+}
+export type WorldviousAnswer = {
+    id: string
+    value?: string[]
+}
+export interface WorldviousFeedbackSnoozeData {
+    kind: WorldviousNotificationKind;
+    id: string;
+    days: number;
+}
+
+export declare type WorldviousNotificationItem = 
+    WorldviousNewVersionInfo | 
+    WorldviousFeedbackRequest |
+    WorldviousMessageData;
+
+export interface WorldviousVersionInfoResult {
+    notifications: WorldviousNotificationItem[];
+}
+
+export interface WorldviousNotificationsInfo {
+    count: number;
+}
